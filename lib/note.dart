21e77d677e8d2b7ac5e59b9ea1ed70acf18086b7
netnapha thaoang <58929759+Netnapha05@users.noreply.github.com>
2023-04-02 19:31:40 +0700

    Add files via upload

---
+++ lib/note.dart
@@ -0,0 +1,10 @@
+class Note {
+  int id;
+  String title;
+  String content;
+  DateTime date_created;
+  DateTime date_last_edited;
+
+  Note(this.id, this.title, this.content, this.date_created,
+      this.date_last_edited);
+}
